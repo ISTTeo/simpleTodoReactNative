2071a82647f643d3cd95eee5b959033d81254d54
Teo <ldptek@gmail.com>
2019-02-12 15:57:29 +0000

    Delete unnecessary HelloWorld program. Add line with counter of todos

--- App.js
+++ App.js
@@ -6,36 +6,20 @@ export default class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      count:0,
       todos:todosData
     }
-    this.handleClick = this.handleClick.bind(this)
   }
 
-  handleClick() {
-    this.setState(prevState => {
-      return {
-        count: prevState.count + 1
-      }
-    })
-  }
 
   render() {
     const todoItems = this.state.todos.map( item =>
       <TodoItem key={item.id} text={item.text}/>
-
     )
 
     return (
       <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <Text>This many clicks: {this.state.count}</Text>
-      <Button
-        onPress = {this.handleClick}
-        title="Click"
-        color="#841584"
-        accessibilityLabel="ClickClickClick"
-      />
+      <Text>This many todos: {todoItems.length}</Text>
+
       {todoItems}
       </View>
     )
